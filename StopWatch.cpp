0b8ecf0593cec5914a1ae661c9ad28f22e518b03
miner90027 <alekszander.mccormick@outlook.com>
2021-01-28 11:08:14 -0900

    Initial commit

---
+++ StopWatch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aleks on 01/28/21.
+//
+
+#include "StopWatch.hpp"
---
+++ StopWatch.hpp
@@ -0,0 +1,14 @@
+//
+// Created by aleks on 01/28/21.
+//
+
+#ifndef HOMEWORK1_STOPWATCH_HPP
+#define HOMEWORK1_STOPWATCH_HPP
+
+
+class StopWatch {
+
+};
+
+
+#endif //HOMEWORK1_STOPWATCH_HPP
