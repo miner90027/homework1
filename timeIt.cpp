0324383c6bcd11b0ab070b1830b4d69d5329b2ae
miner90027 <alekszander.mccormick@outlook.com>
2021-02-02 14:33:51 -0900

    -renamed main.cpp to timeIt.cpp - added code for generating rand numbers between 0 & 100

--- timeIt.cpp
+++ timeIt.cpp
@@ -14,11 +14,18 @@ using std::vector;
 using std::cout;
 using std::endl;
 
+int randNumGen();
 
 int main() {
+	vector<int> intVec(10);
+	intVec.
+
+}
+
+int randNumGen(){
 	random_device randDev;
 	mt19937 gen(randDev());
 	uniform_int_distribution<>dis(0,100);
+	return dis(gen);
 
-	cout << dis(gen) << endl;
 }
